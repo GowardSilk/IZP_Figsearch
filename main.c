de9ea767f6d0ac272f66cdfc9a05e5f12f9f1159
Goward_Silk <megaupfunguj@gmail.com>
2024-11-16 01:24:32 +0100

    Added hline and vline searching #1.0; todo: merge hline and vline with BitmapIterator

--- main.c
+++ main.c
@@ -51,6 +51,10 @@ typedef struct BitmapSize {
     uint32_t height;
 } BitmapSize;
 
+static inline size_t bmp_size_raw(BitmapSize dimension) {
+    return (size_t)dimension.width * dimension.height;
+}
+
 typedef char Pixel;
 typedef Pixel *BitmapData;
 
@@ -62,7 +66,7 @@ typedef struct BitmapRaw {
 } BitmapRaw;
 
 typedef struct Bitmap {
-    size_t size;
+    BitmapSize dimensions;
     BitmapData data;
 } Bitmap;
 
@@ -71,7 +75,8 @@ typedef struct Bitmap {
  * @note constructor invalidates raw bitmap data pointer
  */
 Bitmap bmp_ctor(BitmapRaw *bmp_raw) {
-    Bitmap bmp = (Bitmap){.data = bmp_raw->data, .size = bmp_raw->size};
+    Bitmap bmp =
+        (Bitmap){.data = bmp_raw->data, .dimensions = bmp_raw->dimensions};
     bmp_raw->data = NULL;
     return bmp;
 }
@@ -86,8 +91,9 @@ BitmapRaw bmp_raw_ctor(BitmapSize sz) {
     BitmapRaw raw = {0};
     raw.dimensions = sz;
     raw.size = 0;
-    raw.cap = sz.height * sz.width;
+    raw.cap = (size_t)sz.height * sz.width + 1;
     raw.data = malloc(raw.cap);
+    return raw;
 }
 
 BitmapRaw *bmp_raw_add(BitmapRaw *bmp, Pixel c) {
@@ -99,9 +105,11 @@ BitmapRaw *bmp_raw_add(BitmapRaw *bmp, Pixel c) {
 }
 
 void bmp_raw_dtor(BitmapRaw *bmp) {
-    free(bmp->data);
-    bmp->size = 0;
-    bmp->cap = 0;
+    if (bmp->data) {
+        free(bmp->data);
+        bmp->size = 0;
+        bmp->cap = 0;
+    }
 }
 
 static inline bool valid_whitespace(char c) { return c == ' ' || c == '\n'; }
@@ -115,7 +123,6 @@ Error bmp_ignore_whitespace(FILE *file, BitmapRaw *out_raw) {
         }
         if (valid_pix(c)) {
             if (!bmp_raw_add(out_raw, c)) {
-                printf("Invalid!");
                 return error_ctor(
                     ERR_INVALID_BITMAP_FILE,
                     "The raw bitmap size does not match given dimensions!");
@@ -169,7 +176,12 @@ Error bmp_load_internal(const char *file_name, BitmapRaw *out_bmp) {
     *out_bmp = bmp_raw_ctor(size);
 
     /* filter whitespace from file into the raw bitmap */
-    bmp_ignore_whitespace(file, out_bmp);
+    err = bmp_ignore_whitespace(file, out_bmp);
+    if (err.err) {
+        bmp_raw_dtor(out_bmp);
+        fclose(file);
+        return err;
+    }
 
     /* free resources upon function leave */
     fclose(file);
@@ -191,11 +203,13 @@ Error bmp_load(const char *file_name, BitmapRaw *out_bmp) {
     Error err = bmp_load_internal(file_name, &bmp);
     {
         if (err.err) {
+            error_dispatch(err);
             error_dtor(err);
             return error_ctor(ERR_INVALID_BITMAP_FILE, "Invalid");
         }
         error_dtor(err);
-        return error_ctor(ERR_NONE, "Valid");
+        printf("Valid\n");
+        return error_none();
     }
 }
 
@@ -206,38 +220,195 @@ typedef struct Command {
     const char *file_name;
 } Command;
 
+/**
+ * @brief executes "--help" figsearch command by printing basic data about the
+ * command options */
 Error command_help(void) {
-    printf("Figsearch algorithm\n");
-    printf("-------------------\n");
-    printf("usage: figsearch [command] (optional)[bitmap location]\n");
-    printf("possible commands:\n");
-    printf("\t\t--help\n");
-    printf("\t\ttest\n");
-    printf("\t\thline\n");
-    printf("\t\tvline\n");
-    printf("\t\tsquare\n");
+    printf(
+        "Figsearch algorithm\n"
+        "-------------------\n"
+        "usage: figsearch [command] (optional)[bitmap location]\n"
+        "possible commands:\n"
+        "\t\t--help\n"
+        "\t\ttest\n"
+        "\t\thline\n"
+        "\t\tvline\n"
+        "\t\tsquare\n");
     return error_none();
 }
+
+/**
+ * @brief executes "test" figsearch command by trying to load the bitmap file
+ * @return ERR_INVALID_BITMAP_FILE with message "Invalid"
+ * @see bmp_load when bmp raw param is set to NULL
+ */
 Error command_test(const char *file_name) { return bmp_load(file_name, NULL); }
+
+typedef struct Point {
+    uint32_t x, y;
+} Point;
+
+typedef struct Line {
+    Point begin, end;
+} Line;
+typedef Line VLine;
+typedef Line HLine;
+
+static inline Line line_ctor(Point begin, Point end) {
+    return (Line){begin, end};
+}
+
+static inline void line_print(Line line) {
+    printf("%d %d %d %d\n", line.begin.y, line.begin.x, line.end.y, line.end.x);
+}
+
+static inline uint32_t hline_length(HLine line) {
+    return line.end.x - line.begin.x;
+}
+
+static inline uint32_t vline_length(VLine line) {
+    return line.end.y - line.begin.y;
+}
+
+static inline HLine scan_for_hline_internal(const char *begin, const char *end,
+                                            Point *curr_line) {
+    Point line_start = *curr_line, line_end = *curr_line;
+    curr_line->x++;
+    for (; begin + curr_line->x < end && *(begin + curr_line->x) == '1';
+         curr_line->x++) {
+        line_end.x++;
+    }
+    return line_ctor(line_start, line_end);
+}
+
+/**
+ * @brief scans given row of bitmap for the longest horizontal line
+ * @param out_max is set if and only if any of its initial value is lower than
+ * any on the line
+ */
+void scan_for_hline(const char *begin, const char *end, Point curr_line,
+                    HLine *out_max) {
+    for (; begin + curr_line.x < end; curr_line.x++) {
+        HLine max = scan_for_hline_internal(begin, end, &curr_line);
+        /* compare curr max line with out_max */
+        if (hline_length(*out_max) < hline_length(max)) {
+            /* set new max since out_max is shorter */
+            *out_max = max;
+        }
+    }
+}
+
+/**
+ * @brief executes "hline" figsearch command by printing the longest
+ * [h]orizontal [line] to the stdout
+ */
 Error command_hline(const char *file_name) {
-    printf("Hello from command_hline");
+    /* load bitmap */
     Bitmap bmp = {0};
     {
         BitmapRaw raw = {0};
         Error err = bmp_load(file_name, &raw);
         if (err.err) {
+            bmp_raw_dtor(&raw);
             return err;
         }
         bmp = bmp_ctor(&raw);
     }
-    assert(false);  // TODO
+
+    /* search for longest horizontal line */
+    HLine max = {0};
+    for (uint32_t i = 0; i < bmp.dimensions.height; i++) {
+        scan_for_hline((i * bmp.dimensions.width) + bmp.data,
+                       ((i + 1) * bmp.dimensions.width) + bmp.data,
+                       (Point){.x = 0, .y = i}, &max);
+    }
+
+    /* print results */
+    line_print(max);
+
+    /* cleanup */
+    bmp_dtor(&bmp);
+
     return error_none();
 }
+
+typedef struct BitmapIterator {
+    char *begin;
+    char *end;
+    ptrdiff_t offset;
+} BitmapIterator;
+
+BitmapIterator bmp_it_ctor(char *begin, char *end, ptrdiff_t offset) {
+    return (BitmapIterator){begin, end, offset};
+}
+
+BitmapIterator *bmp_it_move(BitmapIterator *it) {
+    if (it->begin + it->offset < it->end) {
+        it->begin += it->offset;
+    } else {
+        // note: is this fine to "damp" the iterator or should we throw ??
+        it->begin = it->end;
+    }
+    return it;
+}
+
+VLine scan_for_vline_internal(BitmapIterator *it, const uint32_t curr_col,
+                              uint32_t *row_counter) {
+    Point start = {.x = curr_col, .y = *row_counter};
+    Point end = start;
+
+    (*row_counter)++;
+    for (; it->begin < it->end && *(it->begin) == '1'; (*row_counter)++) {
+        bmp_it_move(it);
+        end.y++;
+    }
+
+    return line_ctor(start, end);
+}
+
+void scan_for_vline(BitmapIterator it, const uint32_t curr_col,
+                    VLine *out_max) {
+    uint32_t row_counter = 0;
+    while (it.begin < it.end) {
+        VLine max = scan_for_vline_internal(&it, curr_col, &row_counter);
+        if (vline_length(*out_max) < vline_length(max)) {
+            *out_max = max;
+        }
+        bmp_it_move(&it);
+    }
+}
+
 Error command_vline(const char *file_name) {
-    printf("Hello from command_vline");
-    assert(false);  // TODO
+    /* load bitmap */
+    Bitmap bmp = {0};
+    {
+        BitmapRaw raw = {0};
+        Error err = bmp_load(file_name, &raw);
+        if (err.err) {
+            bmp_raw_dtor(&raw);
+            return err;
+        }
+        bmp = bmp_ctor(&raw);
+    }
+
+    /* search for longest vertical line */
+    VLine max = {0};
+    for (uint32_t i = 0; i < bmp.dimensions.width; i++) {
+        scan_for_vline(
+            bmp_it_ctor(i + bmp.data,
+                        (i + bmp.data) +
+                            (bmp.dimensions.height - 1) * bmp.dimensions.width,
+                        bmp.dimensions.width),
+            i, &max);
+    }
+
+    /* print results */
+    line_print(max);
+
+    /* cleanup */
     return error_none();
 }
+
 Error command_square(const char *file_name) {
     printf("Hello from command_square");
     assert(false);  // TODO
@@ -319,6 +490,8 @@ Error command_parse(int argc, char **argv, Command *out_cmd) {
 int main(int argc, char **argv) {
     /* parse command line */
     Command cmd = {0};
+    argc = 3;
+    argv = (char *[]){"exe", "vline", "../../../build/pic.txt"};
     {
         Error err = command_parse(argc, argv, &cmd);
         if (err.err) {
